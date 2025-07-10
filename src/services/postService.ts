07a5096b2973510a0e99ce2b1dcc786610e4333b
MariaGuerreroR <mypersonalblog255@gmail.com>
2025-07-10 18:50:38 -0500

    Update postService.ts

--- src/services/postService.ts
+++ src/services/postService.ts
@@ -1,9 +1,8 @@
 import axios from 'axios';
 const API_URL = import.meta.env.VITE_API_URL || '/api';
 
-
 const api = axios.create({
-  baseURL: API_URL,
+  baseURL: API_URL + '/api',
 });
 
 api.interceptors.request.use((config) => {
