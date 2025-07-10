0c23de050d74683d84180811eb61e0e2992d3862
MariaGuerreroR <mypersonalblog255@gmail.com>
2025-07-10 18:23:24 -0500

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,20 @@
-/// <reference types="vite/client" />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://blog-backen-5o6c.onrender.com',
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  },
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+});
+
